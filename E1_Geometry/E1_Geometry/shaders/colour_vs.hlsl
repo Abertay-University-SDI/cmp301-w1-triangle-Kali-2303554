0e6f38675ada77598159afb5f37b550658a4e90b
KALI FREW <2303554@uad.ac.uk>
2025-09-17 13:19:24 +0100

    work finished in lab

--- E1_Geometry/E1_Geometry/shaders/colour_vs.hlsl
+++ E1_Geometry/E1_Geometry/shaders/colour_vs.hlsl
@@ -26,14 +26,15 @@ OutputType main(InputType input)
 	OutputType output;
 	
 	// Change the position vector to be 4 units for proper matrix calculations.
-	input.position.w = 1.0f;
+	input.position.w = 0.5f;
     
 	// Calculate the position of the vertex against the world, view, and projection matrices.
 	output.position = mul(input.position, worldMatrix);
 	output.position = mul(output.position, viewMatrix);
 	output.position = mul(output.position, projectionMatrix);
 
-	output.colour = input.colour;
+	//output.colour = input.colour;
+    output.colour = float4(1.f, 0.f, 0.f, 0.f);
 	
 
 	return output;
